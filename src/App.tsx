8905065bf2a7eec0393822ae7190a5894f4ded3b
Egor Domozhirov <egor.domozhirov@aspose.com>
2024-04-04 22:47:34 +0400

    Reorder tasks in column

--- src/App.tsx
+++ src/App.tsx
@@ -12,10 +12,22 @@ const App_: React.FC<Props> = ({
 }) => {
   const [data, setData] = useState<TInitialData>(initialData);
 
-  const onDragEnd = useCallback(() => {
-    debugger;
-    //
-  }, []);
+  const onDragEnd = useCallback((result: any) => {
+    const { destination, source, draggableId } = result;
+
+    if (!destination) return;
+    if (destination.id === source.id && destination.index === source.index) return;
+
+    const column = data.columns[source.droppableId];
+    const newTaskIds = Array.from(column.taskIds);
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    const newColumn = { ...column, taskIds: newTaskIds };
+    const newData = { ...data, columns: { ...data.columns, [newColumn.id]: newColumn } }
+
+    setData(newData);
+  }, [data]);
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
